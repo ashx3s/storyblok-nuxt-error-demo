100b881b9286a3b81207c057934d7c4f4b99664a
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-04-11 14:31:31 -0600

    page text content added

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main>
+    <header>
+      <h1>A Test Repository</h1>
+      <p>For debugging Storyblok Nuxt 3</p>
+    </header>
+    <section>
+      <header>
+        <h2>Error Output</h2>
+        <p>
+          A quick view of the error message. This is done after the node
+          modules folder is removed and then <code>npm i</code> is ran
+          again.
+        </p>
+      </header>
+      <div>
+        <code> </code>
+      </div>
+    </section>
+  </main>
 </template>
+<style>
+h1,
+p {
+  font-family: sans-serif;
+}
+header {
+  display: grid;
+  place-items: center;
+}
+</style>
